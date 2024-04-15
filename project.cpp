742810ee39f826f5cce7ad300de39a54e25547f3
krrishvaghani <164049487+krrishvaghani@users.noreply.github.com>
2024-04-15 14:01:23 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,9 +1,37 @@
-#include<iostream>
+/* Rock - Paper  - scissors Game
+Group Leader = Vaghani Krish
+        members : Sheladiya Mitang
+*/
+#include <iostream>
+#include <ctime>
 
-using namespace std ;
+using namespace std;
 
-int main()
-{
-    cout << "HELLO WORLD " << endl;
-    return 0;
-}
+class rps {
+private:
+    int playerScore;
+    int computerScore;
+
+public:
+    rps() : playerScore(0), computerScore(0) {}
+
+    char getuserchoice() {
+        cout << "Welcome" << endl;
+        cout << "Rock-Paper-Scissors Game!" << endl;
+
+        char player;
+        do {
+            cout << "Choose one of the following" << endl;
+            cout << "***********************************" << endl;
+            cout << " 'r' for rock " << endl;
+            cout << " 'p' for paper " << endl;
+            cout << " 's' for scissors " << endl;
+            cin >> player;
+        } while (player != 'r' && player != 'p' && player != 's');
+        return player;
+    }
+};
+    int main()
+    {
+        return 0;
+    }
